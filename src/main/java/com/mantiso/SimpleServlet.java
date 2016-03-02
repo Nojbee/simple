3b1c630da8e52d82ad61d0c6063e2bf8b0f7e1bf
Nojbee <npalfi88@gmail.com>
2016-03-02 06:24:04 +0100

    c

---
+++ src/main/java/com/mantiso/SimpleServlet.java
@@ -0,0 +1,50 @@
+package com.mantiso;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.lang.String;
+
+/**
+ * Created by Norbi on 2016. 02. 28..
+ */
+@WebServlet(urlPatterns = {"/home", "*.do"}, name = "SimpleServlet", initParams = {@WebInitParam(name = "ProductName", value = "Welcome Application")})
+public class SimpleServlet extends HttpServlet {
+
+    String appName = "My Application";
+
+    @Override
+    public void init() throws ServletException {
+        appName = getServletContext().getInitParameter("ProductName");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (name != null) {
+            resp.setContentType("text/xml");
+            resp.getWriter().printf("<application>" +
+                    "<name>Hello %s</name>" +
+                    "<product>%s</product>" +
+                    "</application>", name, appName);
+        } else {
+            throw new ServletException("A name should be entered");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (name != null && name != "") {
+            resp.getWriter().printf("Hello %s", name);
+        } else {
+            resp.sendRedirect("index.jsp");
+            return;
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="metro-bootstrap-master/css/metro-bootstrap.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<%@include file="_header.jsp"%>
+<section class="main container-fluid">
+    <div class="container">
+        <h1>Home</h1>
+
+        <div class="row-fluid">
+            <div class="col-md-3">Sidebar</div>
+            <div class="col-md-9">
+                <tabset>
+                    <tab heading="Search">
+                        <div>
+                            <form action="home" method="post">
+                                <p>
+                                    Name: <input type="text" name="name"/>
+                                </p>
+
+                                <p>
+                                    <input type="submit" value="Enter name">
+                                </p>
+                            </form>
+                        </div>
+                    </tab>
+                    <tab heading="Next">
+                        Yet More Static content
+                    </tab>
+                </tabset>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
